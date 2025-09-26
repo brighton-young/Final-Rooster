1b9ce20a0b8d928a780526833866e40f2069818b
brighton-young <bright.young@outlook.com>
2025-09-26 19:09:50 -0400

    deploying dist

--- dist/assets/about-legacy-BUaXYApB.js
+++
@@ -1,1 +0,0 @@
-System.register(["./main-legacy-CL3my4dA.js"],function(e,t){"use strict";return{setters:[null],execute:function(){}}});
---
+++ dist/assets/services-legacy-CEQTXPFp.js
@@ -0,0 +1,1 @@
+System.register(["./main-legacy-BPCj25_P.js"],function(e,t){"use strict";return{setters:[null],execute:function(){}}});
